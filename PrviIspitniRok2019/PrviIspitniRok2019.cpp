1d16ad68062fc1dafabb0792c0969a8acf64a419
BornaDobric <BornaDobric@gmail.com>
2019-07-07 01:30:38 +0200

    Update PrviIspitniRok2019.cpp

--- PrviIspitniRok2019/PrviIspitniRok2019.cpp
+++ PrviIspitniRok2019/PrviIspitniRok2019.cpp
@@ -9,45 +9,48 @@
 #define FILE_OPEN_ERROR "\r\nDoslo je do greske prilikom otvaranja datoteke."
 #define STRING_MAX_LENGTH 50
 #define FILENAME "file.txt"
-#define RAND_MIN 10
-#define RAND_MAX 20
+#define RAND_MIN 1
+#define RAND_MAX 5
 
 typedef struct _cvor* Position;
 typedef struct _cvor {
 	int brojIndexa;
+	int orderNum;
 	char ime[STRING_MAX_LENGTH];
 	char prezime[STRING_MAX_LENGTH];
-	int orderNum;
 	Position next;
 }Cvor;
 
-int ReadFromList(Position head);
+int ReadFromFile(Position head);
 int PushElementInList(Position head, Position temp);
-Position CreateNewElement();
-int CreateNewList(Position head, Position lista);
 int FindSpotForSortedInput(Position head, Position temp);
+Position CreateNewElement();
+int MakeNewListWithSortedInput(Position head, Position lista);
+int GetRandomNumber();
 int PrintList(Position head);
 int DeleteAll(Position head);
-int GetRandomNumber();
 
 int main(int argc, char *argv[]) {
 	Cvor head;
 	Cvor lista;
 	char c = 0;
+	srand((unsigned)time(NULL));
 	head.next = NULL;
 	lista.next = NULL;
-	ReadFromList(&head);
+
+	ReadFromFile(&head);
 	PrintList(head.next);
-	printf("\r\nNova lista: \r\n");
-	CreateNewList(&head, &lista);
+
+	printf("\r\n---------------------------\r\nNova lista:\r\n");
+	MakeNewListWithSortedInput(&head, &lista);
 	PrintList(lista.next);
 	scanf(" %c", &c);
 	return 0;
 }
 
-int CreateNewList(Position head, Position lista) {
+int MakeNewListWithSortedInput(Position head, Position lista) {
 	Position temp;
-	while (head->next!=NULL)
+	while (head->next != NULL)
 	{
 		temp = CreateNewElement();
 		if (temp == NULL)
@@ -65,20 +68,7 @@ int CreateNewList(Position head, Position lista) {
 	return 0;
 }
 
-int PushElementInList(Position head, Position temp) {
-	temp->next = head->next;
-	head->next = temp;
-	return 0;
-}
-
-int FindSpotForSortedInput(Position head, Position temp) {
-	while (head->next != NULL && _strcmpi(head->next->ime, temp->ime) > 0)
-		head = head->next;
-	PushElementInList(head, temp);
-	return 0;
-}
-
-int ReadFromList(Position head) {
+int ReadFromFile(Position head) {
 	FILE *fp;
 	Position temp;
 	char buffer[STRING_MAX_LENGTH];
@@ -97,12 +87,31 @@ int ReadFromList(Position head) {
 				if (fgets(buffer,STRING_MAX_LENGTH,fp)!=NULL)
 				{
 					sscanf(buffer, " %d %s %s", &temp->brojIndexa, temp->ime, temp->prezime);
-					FindSpotForSortedInput(head, temp);
+					PushElementInList(head, temp);
+					head = head->next;
 				}
 			}
 		}
 		fclose(fp);
 	}
+	return 0;
+}
+
+int GetRandomNumber() {
+	int broj = (rand() % (RAND_MAX - RAND_MIN + 1) + RAND_MIN);
+	return broj;
+}
+
+int FindSpotForSortedInput(Position head, Position temp) {
+	while (head->next != NULL && _strcmpi(head->next->prezime, temp->prezime) < 0)
+		head = head->next;
+	PushElementInList(head, temp);
+	return 0;
+}
+
+int PushElementInList(Position head, Position temp) {
+	temp->next = head->next;
+	head->next = temp;
 	return 0;
 }
 
@@ -119,6 +128,20 @@ Position CreateNewElement() {
 	return temp;
 }
 
+int PrintList(Position head) {
+	if (head == NULL)
+		printf(EMPTY_LIST);
+	else
+	{
+		while (head!=NULL)
+		{
+			printf(" %s %s %d %d\r\n", head->ime, head->prezime, head->brojIndexa, head->orderNum);
+			head = head->next;
+		}
+	}
+	return 0;
+}
+
 int DeleteAll(Position head) {
 	Position temp;
 	printf("\r\nBrisanje liste...");
@@ -129,24 +152,5 @@ int DeleteAll(Position head) {
 		free(temp);
 	}
 	printf("\r\nLista uspjesno izbrisana!\r\n");
-	return 0;
-}
-
-int GetRandomNumber() {
-	int broj = (rand() % (RAND_MAX - RAND_MIN + 1) + RAND_MIN);
-	return broj;
-}
-
-int PrintList(Position head) {
-	if (head == NULL)
-		printf(EMPTY_LIST);
-	else
-	{
-		while (head!=NULL)
-		{
-			printf(" %s %s redni broj: %d   orderNum: %d\r\n", head->ime, head->prezime, head->brojIndexa, head->orderNum);
-			head = head->next;
-		}
-	}
 	return 0;
 }
