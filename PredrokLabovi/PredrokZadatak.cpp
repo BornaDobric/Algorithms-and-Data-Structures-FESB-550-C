294bfb9b90ed3e9fcde8c209890ca3c948ca11e3
BornaDobric <BornaDobric@gmail.com>
2019-07-10 08:35:04 +0200

    Update PredrokZadatak.cpp

--- PredrokLabovi/PredrokZadatak.cpp
+++ PredrokLabovi/PredrokZadatak.cpp
@@ -1,42 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string>
 #include<string.h>
 #include<time.h>
-#define STRING_MAX_LENGTH 50
 #define ALLOCATION_ERROR "\r\nDoslo je do greske prilikom alokacije memorije!"
 #define EMPTY_LIST "\r\nLista je prazna!"
-#define FILE_OPEN_ERROR "\r\nDOslo je do greske prilikom otvaranja datoteke!"
+#define FILE_OPEN_ERROR "\r\nDoslo je do greske prilikom otvaranja datoteke!"
+#define STRING_MAX_LENGTH 50
+#define FILENAME "studenti2702.txt"
+#define RAND_MIN 1
+#define RAND_MAX 3
 
 typedef struct _cvor* Position;
 typedef struct _cvor {
+	int ocjenaP2;
+	int ocjenaAISP;
 	char ime[STRING_MAX_LENGTH];
 	char prezime[STRING_MAX_LENGTH];
-	int ocjenaP2;
-	int ocjenaAisp;
 	Position next;
 }Cvor;
 
-int ReadGradesFromP2(Position head, char *filename);
-int ReadGradesFromAISP(Position head, char *filename);
+int ReadFromFile(Position head, const char *filename);
+int ReadFromAISP(Position head, const char *filename);
+int PushElement(Position head, Position temp);
 Position CreateNewElement();
-int PushNode(Position head, Position temp);
+int FindSpot(Position head, Position temp);
 int PrintList(Position head);
 int DeleteAll(Position head);
 
 int main(int argc, char *argv[]) {
 	Cvor head;
+	char c = 0;
 	head.next = NULL;
-	ReadGradesFromP2(&head, "P2.txt");
+	ReadFromFile(&head, "P2.txt");
 	PrintList(head.next);
-	ReadGradesFromAISP(&head, "aisp.txt");
+	printf("\r\n---------------------\r\n");
+	ReadFromAISP(&head, "aisp.txt");
 	PrintList(head.next);
 	DeleteAll(&head);
+	scanf(" %c", &c);
 	return 0;
 }
-int ReadGradesFromP2(Position head, char *filename) {
+
+int ReadFromAISP(Position head, const char *filename) {
 	FILE *fp;
 	Position temp;
+	char buffer[STRING_MAX_LENGTH];
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		printf(FILE_OPEN_ERROR);
@@ -47,21 +57,34 @@ int ReadGradesFromP2(Position head, char *filename) {
 			temp = CreateNewElement();
 			if (temp == NULL)
 				printf(ALLOCATION_ERROR);
-			else {
-				fscanf(fp, " %s %s %d", temp->ime, temp->prezime, &temp->ocjenaP2);
-				PushNode(head, temp);
+			else
+			{
+				if (fgets(buffer, STRING_MAX_LENGTH, fp) != NULL)
+				{
+					sscanf(buffer, " %s %s %d", temp->ime, temp->prezime, &temp->ocjenaAISP);
+					FindSpot(head, temp);
+				}
 			}
 		}
 		fclose(fp);
 	}
 	return 0;
 }
-int ReadGradesFromAISP(Position head, char *filename) {
+
+int FindSpot(Position head, Position temp) {
+	while (head->next != NULL && _strcmpi(head->next->prezime, temp->prezime) != 0)
+		head = head->next;
+	if (head->next != NULL && head->next->ocjenaP2 > 1)
+		head->next->ocjenaAISP = temp->ocjenaAISP;
+	else
+		PushElement(head, temp);
+	return 0;
+}
+
+int ReadFromFile(Position head, const char *filename) {
 	FILE *fp;
-	Position temp = head->next;
-	Position newElement;
+	Position temp;
 	char buffer[STRING_MAX_LENGTH];
-	char blah[STRING_MAX_LENGTH];
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		printf(FILE_OPEN_ERROR);
@@ -69,36 +92,43 @@ int ReadGradesFromAISP(Position head, char *filename) {
 	{
 		while (!feof(fp))
 		{
-			fgets(buffer, STRING_MAX_LENGTH, fp);
-			if (temp->ocjenaP2>1)
-			{
-				sscanf(buffer, " %s %s %d", blah, blah, &temp->ocjenaAisp);
-			}
+			temp = CreateNewElement();
+			if (temp == NULL)
+				printf(ALLOCATION_ERROR);
 			else
 			{
-				newElement = CreateNewElement();
-				PushNode(head, newElement);
+				if (fgets(buffer, STRING_MAX_LENGTH, fp) != NULL)
+				{
+					while (head->next != NULL)
+						head = head->next;
+					sscanf(buffer, " %s %s %d", temp->ime, temp->prezime, &temp->ocjenaP2);
+					PushElement(head, temp);
+				}
 			}
-			head = head->next;
 		}
 		fclose(fp);
 	}
 	return 0;
 }
-int PushNode(Position head, Position temp) {
+
+int PushElement(Position head, Position temp) {
 	temp->next = head->next;
 	head->next = temp;
 	return 0;
 }
+
 Position CreateNewElement() {
 	Position temp;
 	temp = (Position)malloc(sizeof(Cvor));
 	if (temp == NULL)
-		printf(ALLOCATION_ERROR);
+		return NULL;
 	else
+	{
 		temp->next = NULL;
+	}
 	return temp;
 }
+
 int PrintList(Position head) {
 	if (head == NULL)
 		printf(EMPTY_LIST);
@@ -106,17 +136,18 @@ int PrintList(Position head) {
 	{
 		while (head!=NULL)
 		{
-			if (head->ocjenaAisp >0 && head->ocjenaP2>0)
-				printf("%s %s - p2:%d	aisp:%d\n", head->ime, head->prezime, head->ocjenaP2, head->ocjenaAisp);
-			else if (head->ocjenaP2 >0)
-				printf("%s %s - p2:%d	\n", head->ime, head->prezime, head->ocjenaP2);
-			else if (head->ocjenaAisp >0)
-				printf("%s %s - aisp:%d	\n", head->ime, head->prezime, head->ocjenaAisp);
+			if (head->ocjenaP2 > 1 && head->ocjenaAISP > 0)
+				printf(" %s %s    ocjena P2: %d    ocjena AISP: %d\r\n", head->ime, head->prezime, head->ocjenaP2, head->ocjenaAISP);
+			else if (head->ocjenaP2 > 0)
+				printf(" %s %s    ocjena P2: %d\r\n", head->ime, head->prezime, head->ocjenaP2);
+			else
+				printf(" %s %s    ocjena AISP: %d\r\n", head->ime, head->prezime, head->ocjenaAISP);
 			head = head->next;
 		}
 	}
 	return 0;
 }
+
 int DeleteAll(Position head) {
 	Position temp;
 	printf("\r\nBrisanje liste...");
@@ -126,6 +157,6 @@ int DeleteAll(Position head) {
 		head->next = temp->next;
 		free(temp);
 	}
-	printf("\r\nLista uspjesno izbrisana!\r\n");
+	printf("\r\nLista je uspjesno izbrisana!\r\n");
 	return 0;
 }
